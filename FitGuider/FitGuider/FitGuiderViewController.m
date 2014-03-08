05ec035429c04fd505c8b3bd5696c1ae1c595bc2
Amelie <amelie20080@gmail.com>
2014-03-08 15:09:09 +0000

    create project

---
+++ FitGuider/FitGuider/FitGuiderViewController.m
@@ -0,0 +1,29 @@
+//
+//  FitGuiderViewController.m
+//  FitGuider
+//
+//  Created by Amelie on 14-3-8.
+//  Copyright (c) 2014年 COMP41550. All rights reserved.
+//
+
+#import "FitGuiderViewController.h"
+
+@interface FitGuiderViewController ()
+
+@end
+
+@implementation FitGuiderViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
