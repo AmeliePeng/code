5d24f258b44a789223820a173bacaa6ad0d45ac9
QishanYang <qishan.yang@ucdconnect.ie>
2014-03-11 10:19:53 +0000

    Add The main page

--- FitGuider/FitGuider/FitGuiderViewController.m
+++ FitGuider/FitGuider/FitGuiderViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "FitGuiderViewController.h"
+#import "SampleCell.h"
+#import "DetailViewController.h"
 
-@interface FitGuiderViewController ()
+#define TABLE_HEIGHT 80
+
+@interface FitGuiderViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, retain) NSMutableArray* arrayForPlaces;
 @end
 
 @implementation FitGuiderViewController
@@ -16,6 +21,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSString* plistPath = [[NSBundle mainBundle] pathForResource:@"SampleData" ofType:@"plist"];
+    NSMutableDictionary *plistDict = [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
+    self.arrayForPlaces = [plistDict objectForKey:@"Data"];
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,6 +33,57 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return TABLE_HEIGHT;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.arrayForPlaces count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    SampleCell *cell = (SampleCell*) [tableView dequeueReusableCellWithIdentifier:@"SampleCell"];
+    if(cell == nil)
+    {
+        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"SampleCell" owner:[SampleCell class] options:nil];
+        cell = (SampleCell *)[nib objectAtIndex:0];
+        cell.contentView.backgroundColor = [UIColor clearColor];
+    }
+    
+    NSDictionary* dict = [self.arrayForPlaces objectAtIndex:indexPath.row];
+    
+    cell.labelForPlace.text = [NSString stringWithFormat:@"%@",[dict objectForKey:@"Place"]];
+    cell.labelForCountry.text = [NSString stringWithFormat:@"%@",[dict objectForKey:@"Country"]];
+    cell.imageview.image = [UIImage imageNamed:[NSString stringWithFormat:@"%@",[dict objectForKey:@"Image"]]];
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    CGRect cellFrameInTableView = [tableView rectForRowAtIndexPath:indexPath];
+    CGRect cellFrameInSuperview = [tableView convertRect:cellFrameInTableView toView:[tableView superview]];
+    
+    DetailViewController* detailViewController = [[DetailViewController alloc]initWithNibName:@"DetailViewController" bundle:[NSBundle mainBundle]];
+    
+    NSMutableDictionary* dict = [self.arrayForPlaces objectAtIndex:indexPath.row];
+    detailViewController.dictForData = dict;
+    detailViewController.yOrigin = cellFrameInSuperview.origin.y;
+    NSLog(@"Self:%@",self.navigationController);
+    
+    [self.navigationController pushViewController:detailViewController animated:NO];
+    //[tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    
+    //detailViewController.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal; [self presentViewController:detailViewController animated:YES completion:nil];
 }
 
 @end
